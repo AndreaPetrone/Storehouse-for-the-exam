1d25a6ce434c9c8a4bbaefbe0c64a4824bfd7b6a
AndreaPetrone <iis.petrone.andrea@gmail.com>
2025-02-12 17:10:52 +0100

    Create assign.R

---
+++ assign.R
@@ -0,0 +1,11 @@
+bmR17<-maradiTC17[[1]] 
+bmG17<-maradiTC17[[2]] 
+bmB17<-maradiTC17[[3]] 
+bmI17<-maradiFC17[[1]] 
+bm17<-c(bmR17,bmG17, bmB17,bmI17) 
+
+bmR24<-maradiTC24[[1]]
+bmG24<-maradiTC24[[2]]
+bmB24<-maradiTC24[[3]]
+bmI24<-maradiFC24[[1]]
+bm24<-c(bmR24,bmG24, bmB24,bmI24)
