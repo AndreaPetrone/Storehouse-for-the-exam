e45720307a0add59c6dd00429961a4465db27bbb
AndreaPetrone <iis.petrone.andrea@gmail.com>
2025-02-12 18:31:43 +0100

    Create dvi.R

---
+++ dvi.R
@@ -0,0 +1,4 @@
+cl1 <- colorRampPalette(c("blue", "grey", "palegoldenrod", "#228B22", "darkgreen"))(100) # To appreciate better the DVI and NDVI 
+
+dvi2017<-bm17[[4]]-bm17[[1]]
+dvi2024<-bm24[[4]]-bm24[[1]]
