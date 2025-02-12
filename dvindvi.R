a76744d93ee87406dadf4af06b9de5d52e160d21
AndreaPetrone <iis.petrone.andrea@gmail.com>
2025-02-12 18:33:09 +0100

    Update and rename dvi.R to dvindvi.R

--- dvindvi.R
+++ dvindvi.R
@@ -2,3 +2,6 @@ cl1 <- colorRampPalette(c("blue", "grey", "palegoldenrod", "#228B22", "darkgreen
 
 dvi2017<-bm17[[4]]-bm17[[1]]
 dvi2024<-bm24[[4]]-bm24[[1]]
+
+ndvi2017 = dvi2017/(bm17[[4]]+bm17[[1]])
+ndvi2024 = dvi2024/(bm24[[4]]+bm24[[1]])
