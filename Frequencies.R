013f35db5df8ed1c5c5a894327aaedcc3f2a29df
AndreaPetrone <iis.petrone.andrea@gmail.com>
2025-02-12 17:57:13 +0100

    Create Frequencies.R

---
+++ Frequencies.R
@@ -0,0 +1,11 @@
+f17 <- freq(Cbm17)
+tot17 <- ncell(Cbm17)  
+p17 = f17*100/tot17
+# Class 1 Restored land = 33%
+# Class 2 City and deserted areas = 67%
+
+f24 <- freq(Cbm24) 
+tot24 <- ncell(Cbm24)
+p24 = f24*100/tot24
+# Class 1 Restored land = 36%
+# Class 2 City and deserted areas = 64%
